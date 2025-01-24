d8e2148d4a922c749ffd69a0ca77cfda44cb5694
aic-6301 <rx435423@gmail.com>
2025-01-24 16:23:33 +0900

    Fix SQL injection vulnerability in database.py

--- utils/database.py
+++ utils/database.py
@@ -26,7 +26,7 @@ def get(table, guild: int):
     """
     with connect() as conn:
         c = conn.cursor()
-        c.execute(f'SELECT * FROM \"{table}\" WHERE guild = ?', (guild,))
+        c.execute('SELECT * FROM \"{}\" WHERE guild = ?'.format(table), (guild,))
         return c.fetchone()
 
 def get_key(table, key, value, key_column='*'):
@@ -35,7 +35,7 @@ def get_key(table, key, value, key_column='*'):
     """
     with connect() as conn:
         c = conn.cursor()
-        c.execute(f'SELECT {key_column} FROM \"{table}\" WHERE \"{key}\" = ?', (value,))
+        c.execute('SELECT {} FROM \"{}\" WHERE \"{}\" = ?'.format(key_column, table, key), (value,))
         return c.fetchall()
 
 def get_all(table):
@@ -44,7 +44,7 @@ def get_all(table):
     """
     with connect() as conn:
         c = conn.cursor()
-        c.execute(f'SELECT * FROM \"{table}\"')
+        c.execute('SELECT * FROM \"{}\"'.format(table))
         return c.fetchall()
 
 def insert(table, columns, values):
@@ -54,7 +54,7 @@ def insert(table, columns, values):
     with connect() as conn:
         c = conn.cursor()
         placeholders = ', '.join(['?'] * len(values))
-        c.execute(f'INSERT INTO \"{table}\" ({", ".join(columns)}) VALUES ({placeholders})', (*values,))
+        c.execute('INSERT INTO \"{}\" ({}) VALUES ({})'.format(table, ", ".join(columns), placeholders), (*values,))
         conn.commit()
 
 
@@ -68,7 +68,7 @@ def insert_or_update(table, columns, values, key_column='guild', key_value=None)
         else:
             c = conn.cursor()
             placeholders = ', '.join(['?'] * len(values))
-            c.execute(f'INSERT INTO \"{table}\" ({", ".join(columns)}) VALUES ({placeholders})', (*values,))
+            c.execute('INSERT INTO \"{}\" ({}) VALUES ({})'.format(table,", ".join(columns), placeholders), (*values,))
             conn.commit()
 
 def update(table, columns, values, key_column='guild', key_value=None):
@@ -78,7 +78,7 @@ def update(table, columns, values, key_column='guild', key_value=None):
     with connect() as conn:
         c = conn.cursor()
         for column, value in zip(columns, values):
-            c.execute(f'UPDATE \"{table}\" SET {column} = ? WHERE {key_column} = ?', (value, key_value))
+            c.execute('UPDATE \"{}\" SET {} = ? WHERE {} = ?'.format(table, column, key_column), (value, key_value))
         conn.commit()
 
 def delete(table, key_column='guild', key_value=None):
@@ -87,7 +87,7 @@ def delete(table, key_column='guild', key_value=None):
     """
     with connect() as conn:
         c = conn.cursor()
-        c.execute(f'DELETE FROM \"{table}\" WHERE \"{key_column}\" = ?', (key_value,))
+        c.execute('DELETE FROM \"{}\" WHERE \"{}\" = ?'.format(table, key_column), (key_value,))
         conn.commit()
 
 def run_sql(sql):
@@ -97,16 +97,3 @@ def run_sql(sql):
     with connect() as conn:
         c = conn.cursor()
         c.execute(sql)
-
-# Specific functions using the helper functions
-def set(table, key, value, guild: int):
-    insert_or_update(table, [key], [value], key_value=guild)
-
-def set_keys(table, *keys, value, guild: int):
-    insert_or_update(table, keys, [guild, value], key_value=guild)
-
-def set_channel(table, value, channel: int, guild: int):
-    insert_or_update(table, ['guild', 'bool', 'channel'], [guild, value, channel], key_value=guild)
-
-def set_channel_role(table, value, role: int, channel: int, guild: int):
-    insert_or_update(table, ['guild', 'bool', 'role', 'channel'], [guild, value, role, channel], key_value=guild)
