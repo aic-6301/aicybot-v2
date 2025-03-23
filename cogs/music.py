627ce8cfccd3b051b5f677ade3b5178a4b7cd4f6
aic-6301 <rx435423@gmail.com>
2025-03-23 10:14:43 +0900

    逆

--- cogs/music.py
+++ cogs/music.py
@@ -25,7 +25,7 @@ class spotify(commands.Cog):
         if m.activities:
             for activity in m.activities:
                 if isinstance(activity, discord.Spotify):
-                    embed = discord.Embed(title=f"{f'{m.display_name}が' if not member else ''}Spotifyで再生中",
+                    embed = discord.Embed(title=f"{f'{m.display_name}が' if member else ''}Spotifyで再生中",
                                           color=discord.Colour(0x1DB954))
                     embed.add_field(name="タイトル", value=activity.title, inline=False)
                     embed.add_field(name="アーティスト", value=f"[{activity.artist}](https://google.com/search?q={activity.artist.replace(' ', '+')})", inline=False)
@@ -41,7 +41,7 @@ class spotify(commands.Cog):
                         if activity.small_image_text != "Playing":
                             await interaction.response.send_message(f"何もしていないようです。", ephemeral = True)
                             return
-                        embed = discord.Embed(title = f"{f'{m.display_name}が' if not member else ''}{activity.name}で再生中", description="これ以上の情報は不明です。", color = discord.Colour(0x1DB954))
+                        embed = discord.Embed(title = f"{f'{m.display_name}が' if member else ''}{activity.name}で再生中", description="これ以上の情報は不明です。", color = discord.Colour(0x1DB954))
                         embed.add_field(name = "タイトル", value = activity.details, inline = False)
                         embed.add_field(name = "詳細", value = activity.state, inline = False)
                         embed.add_field(name = "再生開始時間", value = f"{discord.utils.format_dt(activity.start, 'T')}", inline = False)
@@ -50,7 +50,7 @@ class spotify(commands.Cog):
                         await interaction.response.send_message(embed = embed, ephemeral = True if not member else False)
                         return
                     elif activity.name == 'Spotify':
-                        embed = discord.Embed(title = f"{f'{m.display_name}が' if not member else ''}{activity.name}で再生中", description="これ以上の情報は不明です。\n-# おそらくローカルファイルを再生中です。", color = discord.Colour(0x1DB954))
+                        embed = discord.Embed(title = f"{f'{m.display_name}が' if member else ''}{activity.name}で再生中", description="これ以上の情報は不明です。\n-# おそらくローカルファイルを再生中です。", color = discord.Colour(0x1DB954))
                         embed.add_field(name = "タイトル", value = activity.details, inline = False)
                         if activity.state:
                             embed.add_field(name = "詳細", value = activity.state, inline = False)
